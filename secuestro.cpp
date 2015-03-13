cbd3a2f33abec6dc5ef768c880a78519ecfb8a17
David Castro <david.castro@unitec.edu>
2015-03-13 16:39:24 -0600

    secuestro

---
+++ secuestro.cpp
@@ -0,0 +1,83 @@
+#include "Caso.h"
+#include "secuestro.h"
+#include <string>
+#include <vector>
+#include "Evidencia.h"
+#include "Investigador.h"
+#include <sstream>
+
+using namespace std;
+
+Secuestro::Secuestro(int numCaso,vector<Investigador> Invest,vector<Evidencia> Evidence,string incidente,string fechaIncidente,bool cerrado,string nom_victima,string lugar_s,string motivo,int cant,bool rescate,bool estado)
+	: Caso(numCaso,Invest,Evidence,incidente,fechaIncidente,cerrado){
+		setNom_victima(nom_victima);
+		setLugar_s(lugar_s);
+		setRescate(rescate);
+		setCant(cant);
+		setEstado(estado);
+		setMotivo(motivo);
+	}
+	//Secuestro( const Secuestro&);
+	string Secuestro::toString() const{
+		stringstream ss;
+		string a="NO", n="MUERTO";
+			if(rescate)
+				a="SI"
+			if(estado)
+				n="VIVO"
+		ss<<Caso::toString()<<" Nombre Victima : "<<nom_victima<<"Lugar : "<<lugar_s<<"Rescate : "<<a<<"Cantidad"<<a
+		<<"Motivo : "<<motivo;
+		return ss.str();	
+	}
+
+void Secuestro::setNom_victima(string nom_victima){
+	this->nom_victima=nom_victima;
+}
+
+void Secuestro::setLugar_s(string lugar_s){
+	this->lugar_s=lugar_s;
+}
+
+void Secuestro::setMotivo(string motivo){
+	this->motivo=motivo;
+}
+
+void Secuestro::setCant(int cant){
+	if(rescate){
+		this->cant=cant;
+	}else{
+		this->cant=0;
+	}
+}
+
+void Secuestro::setRescate(bool rescate){
+	this->rescate=rescate;
+}
+
+void Secuestro::setEstado(bool estado){
+	this->estado=estado;
+}
+
+string Secuestro::getNom_victima() const{
+	return nom_victima;
+}
+
+string Secuestro::getLugar_s() const{
+	return lugar_s;
+}
+
+string Secuestro::getMotivo() const{
+	return motivo;
+}
+
+int Secuestro::getCant() const{
+	return cant;
+}
+
+bool Secuestro::getRescate() const{
+	return rescate;
+}
+
+bool Secuestro::getEstado() const{
+	return estado;
+}
---
+++ secuestro.h
@@ -0,0 +1,34 @@
+#ifndef SECUESTRO_H
+#define SECUESTRO_H
+
+#include "Caso.h"
+#include <string>
+#include <vector>
+#include "Evidencia.h"
+#include "Investigador.h"
+
+using namespace std;
+
+class Secuestro : public Caso{
+	string nom_victima,lugar_s,motivo;
+	int cant;
+	bool rescate, estado;
+public:
+	Secuestro(int,vector<Investigador>,vector<Evidencia>,string,string,bool,string,string,string,int,bool,bool);
+	Secuestro( const Secuestro&);
+	virtual string toString() const;
+	void setNom_victima(string);
+	void setLugar_s(string);
+	void setMotivo(string);
+	void setCant(int);
+	void setRescate(bool);
+	void setEstado(bool);
+	string getNom_victima() const;
+	string getLugar_s() const;
+	string getMotivo() const;
+	int getCant() const;
+	bool getRescate() const;
+	bool getEstado() const;
+};
+
+#endif
