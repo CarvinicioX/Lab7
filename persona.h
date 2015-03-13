6c6542da1ef175d2285fdce8ed22e8d912d96ea2
Guillermo LopeZ <guillelopez_13@hotmail.com>
2015-03-13 15:05:30 -0600

    updated

--- persona.h
+++ persona.h
@@ -1,10 +1,26 @@
 #ifndef PERSONA_H
 #define PERSONA_H
+#include <string>
 
-class Persona
-{
+using std::string;
+class Persona{
+        string nombre_real, usuario, password, identidad, birthdate;
+        int edad;
 public:
-    Persona();
+    Persona(string, string, string, int, string, string);
+    string setNombre(string);
+    string setUsuario(string);
+    string setPassword(string);
+    string setBirthdate(string);
+    Persona(const Persona&);
+    int setEdad(int);
+    void getNombre()const;
+    void getUsuario()const;
+    void getPassword()const;
+    void getId()const;
+    void getBirthdate()const;
+    string toString()const;
+
 };
 
 #endif // PERSONA_H
