c4be24e90fa08500e7c2494d1bc2705785884ca3
CarvinicioX <cavinicio@yahoo.com>
2015-03-13 16:07:29 -0600

    Caso.h Agregado

---
+++ caso.h
@@ -0,0 +1,34 @@
+#ifndef CASOS_H
+#define CASOS_H
+
+#include <string>
+#include <vector>
+#include "Investigador.h"
+#include "Evidencia.h"
+
+using std::string;
+using std::vector;
+
+class Caso {
+	int numCaso;
+	vector <Investigador> Invest;
+	vector <Evidencia> Evidence;
+	string incidente, fechaIncidente;
+	bool cerrado;
+public:
+	Caso(int, vector<Investigador>, vector<Evidencia>, string, string, bool);
+	Caso(const Caso&);
+	void setNumCaso(int);
+	void setInvestigador(Investigador);
+	void setEvidencia(Evidencia);
+	void setIncidente(string);
+	void setFechaIncidente(string);
+	void setCerrado(bool);
+	int getNumCaso()const;
+	Investigador getInvestigador()const;
+	Evidencia getEvidencia()const;
+	string getIncidente()const;
+	string getFechaIncidente()const;
+	bool getCerrado()const;
+	virtual string toString()const;
+}
