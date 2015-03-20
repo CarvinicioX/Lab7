005e845aed7c3895ae4034a5f4a56ad4aece2b65
David Castro <david.castro@unitec.edu>
2015-03-20 13:30:03 -0600

    Investigador bitches

--- Investigador.cpp
+++ Investigador.cpp
@@ -11,10 +11,34 @@ Investigador::Investigador(string nombre_real, string usuario, string password, 
 		setCerrados(cerrados);
 		setSin_resolver(sin_resolver);
 	}
-	Investigador::toString() const;
-	void setAbiertos(int);
-	void setCerrados(int);
-	void setSin_resolver(int);
-	int getAbiertos() const;
-	int getCerrados() const;
-	int getSin_resolver() const;
+
+	Investigador::toString() const{
+		stringstream ss;
+		ss<<Persona::toString()<<"Casos Abiertos: "<<abiertos<<"Casos Cerrados: "<<cerrados<<"Casos sin Resolver: "
+		<<sin_resolver;
+		return ss.str(); 
+	}
+
+	void Investigador::setAbiertos(int abiertos){
+		this->abiertos=abiertos;
+	}
+
+	void Investigador::setCerrados(int cerrados){
+		this->cerrados=cerrados;
+	}
+
+	void Investigador::setSin_resolver(int){
+		this->sin_resolver=sin_resolver;
+	}
+
+	int Investigador::getAbiertos() const{
+		return abiertos;
+	}
+
+	int Investigador::getCerrados() const{
+		return cerrados;
+	}
+
+	int Investigador::getSin_resolver() const{
+		return sin_resolver;
+	}
